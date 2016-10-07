ca6c94d04be9ab9d1917a2c6eb246070ac81154a
Raphael Benitte <benitte@ekino.com>
2016-10-07 20:26:07 +0900

    feat(hover): expose HoverMorphIcon component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-export { default as MorphIcon }   from './components/MorphIcon'
-export { default as CloseButton } from './components/buttons/CloseButton'
-export { default as PlusButton }  from './components/buttons/PlusButton'
-export { default as NavButton }   from './components/buttons/NavButton'
+export { default as MorphIcon }      from './components/MorphIcon'
+export { default as CloseButton }    from './components/buttons/CloseButton'
+export { default as PlusButton }     from './components/buttons/PlusButton'
+export { default as NavButton }      from './components/buttons/NavButton'
+export { default as HoverMorphIcon } from './components/HoverMorphIcon'
